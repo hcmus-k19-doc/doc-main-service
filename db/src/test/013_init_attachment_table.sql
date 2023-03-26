24e7ecaca7a63f8e2db5211bbc80916b730dbaf9
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-03-26 10:21:27 +0700

    [DOC-112] Insert attachment table

---
+++ db/src/test/013_init_attachment_table.sql
@@ -0,0 +1,14 @@
+SET SEARCH_PATH TO doc_main;
+
+CREATE TABLE "attachment"
+(
+    "id"    SERIAL       NOT NULL,
+    "incoming_doc_id" BIGINT NOT NULL,
+    "alfresco_file_id" VARCHAR(255) NOT NULL,
+    "alfresco_folder_id" VARCHAR(255) NOT NULL,
+    "file_type" VARCHAR(255) NOT NULL,
+    CONSTRAINT "attachment_pk" PRIMARY KEY ("id"),
+    CONSTRAINT "attachment_incoming_document_fk" FOREIGN KEY ("incoming_doc_id")
+        REFERENCES "incoming_document" ("id") MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+) INHERITS ("doc_base_table");
