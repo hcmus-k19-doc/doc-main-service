9ad6d021725083de1c1340c2ec79af6a0c96f12b
Thanh Hoang <thanhhoang4869@gmail.com>
2023-05-16 01:44:28 +0700

    [DOC-190] Resolve conflicts

--- web/src/main/java/edu/hcmus/doc/mainservice/controller/OutgoingDocumentController.java
+++ web/src/main/java/edu/hcmus/doc/mainservice/controller/OutgoingDocumentController.java
@@ -10,7 +10,15 @@ import edu.hcmus.doc.mainservice.model.entity.OutgoingDocument;
 import edu.hcmus.doc.mainservice.service.OutgoingDocumentService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @RestController
