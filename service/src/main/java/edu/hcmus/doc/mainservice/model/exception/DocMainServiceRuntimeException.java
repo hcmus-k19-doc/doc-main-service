3d233853ce32a74ebf3bc89370d017160bc849da
Nguyen Duc Nam <namworkmc@gmail.com>
2023-06-07 22:17:41 +0700

    [DOC-214] Migrate Alfresco to AWS S3

--- service/src/main/java/edu/hcmus/doc/mainservice/model/exception/DocMainServiceRuntimeException.java
+++ service/src/main/java/edu/hcmus/doc/mainservice/model/exception/DocMainServiceRuntimeException.java
@@ -2,6 +2,10 @@ package edu.hcmus.doc.mainservice.model.exception;
 
 public class DocMainServiceRuntimeException extends RuntimeException {
 
+  public static final String CONCURRENT_UPDATE = "doc.exception.concurrent_update";
+
+  public static final String INTERNAL_SERVER_ERROR = "doc.exception.internal_server_error";
+
   public DocMainServiceRuntimeException(String message) {
     super(message);
   }
