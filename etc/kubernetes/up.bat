a8d8188eb12dd6dd25f09f81138607bc35f494f9
Nguyen Duc Nam <namworkmc@gmail.com>
2023-06-14 22:07:08 +0700

    [DOC-213] change routing config

--- etc/kubernetes/up.bat
+++ etc/kubernetes/up.bat
@@ -2,16 +2,16 @@ kubectl apply -f ./ingress/doc-kong.yaml
 kubectl apply -f ./ingress/doc-ingress.yaml
 
 kubectl apply -f ./doc-keycloak/doc-keycloak.config.yaml
-kubectl apply -f ./doc-keycloak/doc-keycloak.deployment.yaml
 kubectl apply -f ./doc-keycloak/doc-keycloak.secret.yaml
+kubectl apply -f ./doc-keycloak/doc-keycloak.deployment.yaml
 
 kubectl apply -f ./doc-db/doc-db.config.yaml
 kubectl apply -f ./doc-db/doc-db.secret.yaml
 
 kubectl apply -f ./doc-rabbitmq/doc-rabbitmq.config.yaml
-kubectl apply -f ./doc-rabbitmq/doc-rabbitmq.deployment.yaml
 kubectl apply -f ./doc-rabbitmq/doc-rabbitmq.secret.yaml
+kubectl apply -f ./doc-rabbitmq/doc-rabbitmq.deployment.yaml
 
 kubectl apply -f ./doc-main-service/doc-main-service.config.yaml
-kubectl apply -f ./doc-main-service/doc-main-service.deployment.yaml
 kubectl apply -f ./doc-main-service/doc-main-service.secret.yaml
+kubectl apply -f ./doc-main-service/doc-main-service.deployment.yaml
